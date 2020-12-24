d6fd0bc41fbc9cba642e683f3efe0ac31e5733e1
User <dewhole@ubuntu.ubuntu-domain>
2020-12-24 12:51:29 +0700

    withHiddenPage

--- tenders.py
+++ tenders.py
@@ -8,7 +8,7 @@ import transliterate
 import datetime
 import dataAutorize
 import tenders
-
+from multiprocessing import Pool
 
 
 now = datetime.datetime.now()
@@ -29,21 +29,6 @@ responce = session.post(link, data=data, headers=header).text
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0', 'accept': '*/*'}
 
-# Ajax-параметры
-HEADERSajax = {
-    'Accept': 'text/html, */*',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Connection': 'keep-alive',
-    'Content-Length': '7',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Cookie': '_ym_uid=1608004265188028299; _ym_d=1608004265; _fbp=fb.1.1608004265021.1214446099; viewError=; PHPSESSID=2j0pq2k1lqapucpt1s0ujoiqb0',
-    'Host': 't1.torgi223.ru',
-    'Origin': 'http://t1.torgi223.ru',
-    'Referer': 'http://t1.torgi223.ru/index.php?module=71&mode=viewRequest&requestId=3864',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest',
-}
 
 
 
@@ -52,7 +37,7 @@ def get_html(url, params=None):
     return r
 
 
-def get_ajax(ajaxhref, ajaxId, params=None):
+def get_ajax(ajaxhref, ajaxId, HEADERSajax, params=None):
     d = session.post(ajaxhref, data=ajaxId, headers=HEADERSajax, params=params)
     return d
 
@@ -69,7 +54,7 @@ def get_pages_count(html):
         pagination = paginationTo.find_all('a') 
         pagination2 = str(pagination[-1].get('href'))
         pagination3 = pagination2[21:]
-        return int(pagination3)  
+        return 1 
 
     else:
         return 1
@@ -99,10 +84,12 @@ def get_content(html):
             if word.lower() in nameProcedure2.lower():
                 # Ссылка на процедуру
                 href = item.find('a').get('href')
+                hrefLot = href
                 print(href)
                 print(nameProcedure2)
                 html2 = get_html(href)
-                soup2 = BeautifulSoup(html2.text, 'html.parser')      
+                soup2 = BeautifulSoup(html2.text, 'html.parser')  
+
 
 
 
@@ -129,51 +116,80 @@ def get_content(html):
                 typeProcedure = item.find('td', class_='row-typeTorgsName').get_text(strip=True)         
 
                 # Статус процедуры
-                typeProcedure = item.find('td', class_='row-statusName').get_text(strip=True)         
+                statusProcedure = item.find('td', class_='row-statusName').get_text(strip=True)         
 
                 # Получаем id ajax запроса
                 try:
                     ajaxId0 = soup2.find('td', class_='viewDatanLot').get('lotid')
-                    print('try' + ajaxId0)
+                    print('try ' + ajaxId0)
 
                 except:
                     try:
                         ajaxId0 = soup2.find('td', class_='viewDatanLotPriceRequest').get('lotid')
-                        print('except' + ajaxId0) 
+                        print('except ' + ajaxId0) 
 
                     except:
                         try:
                             ajaxId0 = soup2.find('td', class_='viewDatanLotAuction').get('lotid')
-                            print('else' + ajaxId0)
-
+                            print('except2 ' + ajaxId0)
                         except:
-                            print('error')
-                
+                            try:
+                                ajaxId0 = soup2.find('td', class_='viewDatanLotEShop').get('lotid')
+                                print('except3 ' + ajaxId0)
+                            except:
+                                try:
+                                    ajaxId0 = soup2.find('td', class_='viewDatanLotMlpdo').get('lotid')
+                                    print('except4 ' + ajaxId0)                                
+                                except:
+                                    print('error')
+
 
                 ajaxId = {
                     'id': ajaxId0
                 }
-                ajaxhref = 'http://t1.torgi223.ru/includes/QuotationNt/ajax/qntAjaxViewLotQuotation.php'
-                html3 = get_ajax(ajaxhref, ajaxId)
-                soup3 = BeautifulSoup(html3.text, 'html.parser')
-                print(soup3)
+
+                HEADERSajax = {
+                    'Accept': 'text/html, */*',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                    'Connection': 'keep-alive',
+                    'Content-Length': '8',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Cookie': '_ym_uid=1608004265188028299; _ym_d=1608004265; _fbp=fb.1.1608004265021.1214446099; viewError=; PHPSESSID=m6jfdgp7dlqtp6qpltlv8k4ul2',
+                    'Host': 't1.torgi223.ru',
+                    'Origin': 'http://t1.torgi223.ru',
+                    'Referer': 'http://t1.torgi223.ru/index.php?module=71&mode=viewRequest&requestId=20815',
+                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest',
+                }
+               
+                ajaxhref = 'http://t1.torgi223.ru/qrAjaxViewLotQuotation.php'
+                html3 = get_ajax(ajaxhref, ajaxId, HEADERSajax)
+                soup3 = BeautifulSoup(html3.text, 'html.parser').get_text
+                print (soup3)
+         
+
+
+
+                catalog.append({
+                    'hrefLot': hrefLot,
+                    'numberProcedure': numberProcedure,
+                    'numberEIS': numberEIS,
+                    'customer': customer,
+                    'nameProcedure': nameProcedure2,
+                    'cost': cost,
+                    'date': date,
+                    'dateLast': dateLast,
+                    'typeProcedure': typeProcedure,
+                    'statusProcedure': statusProcedure,
+                    'hiddenText': soup3,
+                })        
 
             else:
                 continue
                
 
-            catalog.append({
-                'numberProcedure': numberProcedure,
-                'numberEIS': numberEIS,
-                'customer': customer,
-                'nameProcedure': nameProcedure2,
-                'cost': cost,
-                'date': date,
-                'dateLast': dateLast,
-                'typeProcedure': typeProcedure,
-
-
-            })            
+    
             
     return catalog                    
             
@@ -186,9 +202,11 @@ def get_content(html):
 def save_file(items, path):
     with open(path, 'w',  encoding='utf8', newline='') as file:
         writer = csv.writer(file, delimiter=',')
-        writer.writerow(['Категории',])
+        writer.writerow(['Ссылка на тендер', 'Номер процедуры', 'Номер извещения в ЕИС', 'Наименование заказчика', 'Наименование процедуры', 'Начальная цена процедуры','Дата публикации', 'Дата окончания подачи заявок', 'Тип процедуры', 'Статус процедуры', 'Скрытый текст'])
         for item in items:
-            writer.writerow([item['title'],])
+            writer.writerow([item['hrefLot'], item['numberProcedure'], item['numberEIS'], item['customer'], item['nameProcedure'], item['cost'], item['date'], item['dateLast'], item['typeProcedure'], item['statusProcedure'], item['hiddenText']])
+
+
 
 # Основная функция Создаём каталог
 def parse1():
@@ -199,7 +217,6 @@ def parse1():
     ]:
 
         html = get_html(URL)
-        print(html)
         if html.status_code == 200:
             catalog = []
             pages_count = get_pages_count(html.text)
@@ -222,6 +239,8 @@ def parse1():
 
 
 
+
+
 parse1()
 #parser2()
 
@@ -234,6 +253,14 @@ parse1()
 'https://2676270.ru/catalog/yarn/2453/',
 'https://2676270.ru/catalog/yarn/909/',
 'https://2676270.ru/catalog/yarn/95/' 
+
+while True:
+    try:
+        next_button = driver.find_element_by_id('oq-nav-nxt')
+    except NoSuchElementException:
+        break
+    next_button.click()
+
 
 while True:
     time.sleep(30)
